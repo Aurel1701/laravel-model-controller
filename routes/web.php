6308e6f0e49f5dda4537f75c2a7671478cc88cd2
Aurel1701 <gantzfan96@gmail.com>
2022-06-13 17:44:59 +0200

    add movie

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/principal', 'FilmController@index')->name('film');
+Route::get('/principal', 'PageController@index')->name('film');
